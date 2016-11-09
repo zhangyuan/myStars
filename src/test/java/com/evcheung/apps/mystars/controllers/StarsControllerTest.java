3d042fdb0a6c116035eed57bd52405a9206bffaa
Yuan Cheung <zhangyuan@users.noreply.github.com>
2016-11-09 23:30:31 +0800

    fix tests by clean dirty context for each test

--- src/test/java/com/evcheung/apps/mystars/controllers/StarsControllerTest.java
+++ src/test/java/com/evcheung/apps/mystars/controllers/StarsControllerTest.java
@@ -9,15 +9,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.HashMap;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class StarsControllerTest  extends ControllerTest{
     @Autowired
     GitHubProvider githubProvider;
